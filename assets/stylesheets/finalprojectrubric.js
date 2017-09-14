fa47067c87812e1e00c61ce872ec79662f494388
emaraosman <emaraosman@gmail.com>
2017-09-13 23:38:49 -0400

    adding content pages and links

---
+++ assets/stylesheets/finalprojectrubric.js
@@ -0,0 +1,49 @@
+/*
+
+FINAL PROJECT RUBRIC
+
+
+Assignment: Final Project
+Consulting Project
+Due Date
+
+Assignment Description
+In this assignment you will act as a consultant to an entity who wants to promote their product. It is your job to use the skills you have learned in order to build a fully functional website designed to promote the product. Be creative, form stories behind the organization; make good use of your skills and don’t be afraid to think outside the box.  The theme of the Web site is decided by the members of your group. Each group will have 2 members, and each member should contribute equally to the project.
+Requirements
+Must be submitted with a wireframe, projects will not be accepted without one
+
+Website should include about 10 pages (minimum 7 pages)
+Website should include an extensive description of the entity and their product.
+A clear HTML format should be used.
+Each page should include 2+ headings.
+ID’s and Classes should be used.
+Use at least 2 font customization methods shown in CSS.
+You should use one external stylesheet. The use of an internal stylesheets in different pages is optional. The external stylesheet should contain the rules which are common in more than 2 or more pages. The internal stylesheet should contain rules which are used only in one particular page.
+The site should show consistency between each of those pages with similar or the same style.
+The CSS positioning property should be used.
+Must include the use of flex-box
+Your code should include comments for organizational purposes.
+Each page should have a unique title (using the same title for all the pages is not considered unique).
+Users should be able to easily navigate through all pages from the original page.
+You should organize all the pages in a good structure.
+You should be using a <div> to layout your pages.(Remember the Box Model, add padding and alignment to your overall page)
+The text, border, background etc. should be contrasting so the page is easier to read. Don't use too many different colors.
+You should use at least 3 of the JavaScript techniques that were introduced.
+Arrays, loops, conditionals, setInterval etc..
+Website should include a minimum of 5 images. You're also encouraged to add some multi-media contents such as a video and hyperlink the images to outside sources or a page you’ve created.
+The use of UL or OL should be used. (Use it however you’d like)
+Should include a modal
+If you're using an image or some text from somewhere else, you must clarify the origin of that resource in a comment in your HTML or CSS file, right before the resource you're using
+The use of CSS or scripts developed by others is ONLY allowed if you have modified it to fit your website. Copying is not allowed, and you will automatically get a zero if such action is detected.
+The use of JQUERY is encouraged but not mandated.
+
+Submission
+Students will meet with me prior to the submission of the project. I will give pointers and critique for final product. I will ask a few question about the HTML, CSS & JavaScript code you wrote, so if you copied something from another website without understanding how it works, you'll get caught very easily.
+Project Ideas
+A photographer / designer and create a website promoting their work.
+A website of an organization/association you know or you are involved with.
+A website for a music artist / athlete
+A hobby or activity you’re interested in.
+
+BE CREATIVE WITH THE CONTENT YOU USE. USE OF PERSONAL RECORDED VIDEOS & IMAGES ARE ENCOURAGE.
+*/
